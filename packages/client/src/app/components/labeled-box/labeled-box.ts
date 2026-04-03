a5b0d0eed6f6002d0858918f1a976dc5770dfd82
Dennis Weiershaeuser <dennis.weiershaeuser@gmail.com>
2026-04-03 13:21:12 +0200

    refactor labeled-box for aria

--- packages/client/src/app/components/labeled-box/labeled-box.ts
+++ packages/client/src/app/components/labeled-box/labeled-box.ts
@@ -1,10 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { NgTemplateOutlet } from '@angular/common'
+import { ChangeDetectionStrategy, Component, input } from '@angular/core'
 
 @Component({
   selector: 'app-labeled-box',
-  imports: [],
+  imports: [NgTemplateOutlet],
   templateUrl: './labeled-box.html',
   styleUrl: './labeled-box.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LabeledBox {}
+export class LabeledBox {
+  readonly containerTag =
+    input<Extract<keyof HTMLElementTagNameMap, 'div' | 'section' | 'nav'>>('div')
+  readonly heading = input.required<string>()
+  readonly headingId = input<string>()
+}
