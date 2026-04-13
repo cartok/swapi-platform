aea7e1da68393b4d983604f180a31b23d779c795
Dennis Weiershaeuser <dennis.weiershaeuser@gmail.com>
2026-04-13 16:55:51 +0200

    create reusable image component with placeholder and fallback rendering, which is ngOptimizedImage compliant

---
+++ packages/client/src/app/components/image/image.ts
@@ -0,0 +1,49 @@
+import { NgOptimizedImage } from '@angular/common'
+import {
+  booleanAttribute,
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  signal,
+} from '@angular/core'
+
+@Component({
+  selector: 'app-image',
+  imports: [NgOptimizedImage],
+  templateUrl: './image.html',
+  styleUrl: './image.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[class.app-image-fill]': 'fill()',
+  },
+})
+export class Image {
+  readonly ngSrc = input.required<NgOptimizedImage['ngSrc']>()
+  readonly alt = input.required<HTMLImageElement['alt']>()
+  readonly priority = input<NgOptimizedImage['priority'], unknown>(false, {
+    transform: booleanAttribute,
+  })
+  readonly fill = input<NgOptimizedImage['fill'], unknown>(false, {
+    transform: booleanAttribute,
+  })
+  readonly width = input<NgOptimizedImage['width']>(undefined)
+  readonly height = input<NgOptimizedImage['height']>(undefined)
+  readonly draggable = input<HTMLImageElement['draggable'], unknown>(false, {
+    transform: booleanAttribute,
+  })
+  readonly placeholder = input(
+    '/assets/svg-sprite-sheet/build/sprite-sheet.1.svg#image-placeholder',
+  )
+
+  readonly loaded = signal(false)
+  readonly loadError = signal(false)
+
+  onImageLoad(): void {
+    this.loaded.set(true)
+  }
+
+  onImageError(): void {
+    this.loaded.set(false)
+    this.loadError.set(true)
+  }
+}
