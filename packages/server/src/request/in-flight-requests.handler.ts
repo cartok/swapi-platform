ea938a715f867a2220a2260f9375674aa13de2dc
Dennis Weiershaeuser <dennis.weiershaeuser@gmail.com>
2026-04-26 23:23:04 +0200

    server: error when potentialy exceeding fly's soft limit

--- packages/server/src/request/in-flight-requests.handler.ts
+++ packages/server/src/request/in-flight-requests.handler.ts
@@ -1,8 +1,19 @@
+import { env } from '#internal/env'
 import type { Handler } from '#internal/types'
 
 export const addInFlightRequestsHandler: Handler = (hono, runContext) => {
   hono.use('*', async (_c, next) => {
     runContext.hono.inFlightRequests++
+
+    if (runContext.hono.inFlightRequests > env.SWAPI_FLY_REQUEST_SOFT_LIMIT) {
+      console.error(
+        [
+          `In-flight requests (${runContext.hono.inFlightRequests}) exceeded`,
+          `fly's soft-limit (${env.SWAPI_FLY_REQUEST_SOFT_LIMIT}).`,
+        ].join(' '),
+      )
+    }
+
     try {
       return await next()
     } finally {
