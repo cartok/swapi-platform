f6b26c18bfa4053e502f07b2e6ec671a6c8a5dd1
Dennis Weiershaeuser <dennis.weiershaeuser@gmail.com>
2026-06-15 14:40:40 +0200

    chore: document supported file types of prettier

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,11 @@
 // @ts-check
 /**
  * @see https://prettier.io/docs/en/configuration.html
+ * Supported file types:
+ * - JavaScript / TypeScript:   .js, .ts, .jsx, .tsx, .mjs, .cjs, .mts, .cts
+ * - Web & Markup:              .html, .vue, .md
+ * - Styles & Layout:           .css, .scss, .less, .mjml
+ * - Templating:                .json, .yaml, .yml, .graphql
  * @type {import("prettier").Config}
  */
 const config = {
